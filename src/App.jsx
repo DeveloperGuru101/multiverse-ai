d9bd757b29ae506ee838946d92182d9716f7f061
DeveloperGuru101 <developerguru72@gmail.com>
2026-03-12 21:05:23 +0530

    app.jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ import logo from "./assets/logo.png";
 export default function App() {
   useEffect(() => {
     // Redirect when page loads
-    window.location.href = "https://warriorplus.com/o2/a/hffvq05/0";
+    const path = window.location.pathname;
+    
+    if (path === "/ghl" || path === "/automation") {
+      window.location.href = "https://www.gohighlevel.com/?fp_ref=guruprasad57";
+    } else {
+      window.location.href = "https://warriorplus.com/o2/a/hffvq05/0";
+    }
   }, []);
   // return (
   //   <div className="app">
