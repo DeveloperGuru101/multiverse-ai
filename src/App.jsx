a8b33272f5a16ee3122c07984c8da6e5a042f347
Guru Prasad Jhinkoo Saroj <guru.saroj@nusummit.com>
2026-05-07 09:47:40 +0530

    book sales rocket code added

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ export default function App() {
   useEffect(() => {
     // Redirect when page loads
     const path = window.location.pathname;
-    
+
     if (path === "/ghl" || path === "/automation") {
       window.location.href = "https://www.gohighlevel.com/?fp_ref=guruprasad57";
-    } else {
+    } else if (path === "/book-sales-rocket") {
+      window.location.href = "https://warriorplus.com/o2/a/j4cr1bz/0";
+    }
+    else {
       window.location.href = "https://warriorplus.com/o2/a/hffvq05/0";
     }
   }, []);
